317bc4c0a76f51f56150da2e5b0a6d1a5fb7c0a4
Bert Verhelst <verhelstbert@gmail.com>
2014-09-28 01:25:10 +0200

    updated manifest

--- PlaylistDownloader/SilentUpdater/Program.cs
+++ PlaylistDownloader/SilentUpdater/Program.cs
@@ -16,27 +16,57 @@ namespace SilentUpdater
 		private static readonly Regex VERSION_DIRECTORY_REGEX = new Regex(@"[0-9]+(\.[0-9]*)?");
 		private static string _newVersionFilePath;
 		private static Manifest _manifest;
+		private static bool _currentVersionExistsLocally;
 
 		static void Main()
 		{
 			try
 			{
 				//start latest local version
-				string currentVersionExe =
-					Path.GetFullPath(Path.Combine(Settings.Default.CurrentVersion, Settings.Default.ExecutablePath));
-				if (File.Exists(currentVersionExe))
-					Process.Start(currentVersionExe);
+				ExecutePlaylistDownloader();
 
 				//check online if new version is availible
 				_manifest = GetManifest();
 
-				if (double.Parse(_manifest.LatestVersion) > double.Parse(Settings.Default.CurrentVersion))
+				if (double.Parse(_manifest.LatestVersion) > double.Parse(Settings.Default.CurrentVersion) || !_currentVersionExistsLocally)
 				{
 					//update is needed
 
 					//download new version zip
 					_newVersionFilePath = Path.GetFullPath(_manifest.LatestVersion + ".zip");
-					DownloadNewVersion(_manifest.DistUrl, _newVersionFilePath);
+					DownloadFile(new Uri(_manifest.DistUrl), _newVersionFilePath);
+
+					//unzip new version
+					Zipper.ExtractZipFile(_newVersionFilePath, Path.GetFullPath(_manifest.LatestVersion));
+					string lastVersion = Settings.Default.CurrentVersion;
+					Settings.Default.CurrentVersion = _manifest.LatestVersion;
+					Settings.Default.Save();
+					File.Delete(_newVersionFilePath);
+
+					//remove older versions
+					List<string> versionDirs = Directory.GetDirectories(".")
+						.Select(Path.GetFileName)
+						.Where(d => VERSION_DIRECTORY_REGEX.IsMatch(d))
+						.OrderByDescending(double.Parse)
+						.Except(new[] { lastVersion, Settings.Default.CurrentVersion })
+						.ToList();
+
+					foreach (string versionDir in versionDirs)
+					{
+						try
+						{
+							Directory.Delete(versionDir);
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+					}
+
+					if (!_currentVersionExistsLocally)
+					{
+						ExecutePlaylistDownloader();
+					}
 				}
 			}
 			catch (Exception ex)
@@ -45,35 +75,17 @@ namespace SilentUpdater
 			}
 		}
 
-		private static void DownloadNewVersion(string url, string filePath)
+		private static void ExecutePlaylistDownloader()
 		{
-			new WebClient().DownloadFile(new Uri(url), filePath);
-
-			//unzip new version
-			Zipper.ExtractZipFile(_newVersionFilePath, Path.GetFullPath(_manifest.LatestVersion));
-			string lastVersion = Settings.Default.CurrentVersion;
-			Settings.Default.CurrentVersion = _manifest.LatestVersion;
-			Settings.Default.Save();
-			File.Delete(_newVersionFilePath);
-
-			//remove older versions
-			List<string> versionDirs = Directory.GetDirectories(".")
-				.Select(Path.GetFileName)
-				.Where(d => VERSION_DIRECTORY_REGEX.IsMatch(d))
-				.OrderByDescending(double.Parse)
-				.Except(new[]{lastVersion, Settings.Default.CurrentVersion})
-				.ToList();
-
-			foreach (string versionDir in versionDirs)
+			string currentVersionExe = Path.GetFullPath(Path.Combine(Settings.Default.CurrentVersion, Settings.Default.ExecutablePath));
+			if (File.Exists(currentVersionExe))
 			{
-				try
-				{
-					Directory.Delete(versionDir);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
+				_currentVersionExistsLocally = true;
+				Process.Start(currentVersionExe);
+			}
+			else
+			{
+				_currentVersionExistsLocally = false;
 			}
 		}
 
@@ -109,6 +121,29 @@ namespace SilentUpdater
 				return data;
 			}
 			return null;
+		}
+
+		private static void DownloadFile(Uri url, string outputFilePath)
+		{
+			const int BUFFER_SIZE = 16 * 1024;
+			using (var outputFileStream = File.Create(outputFilePath, BUFFER_SIZE))
+			{
+				var req = WebRequest.Create(url);
+				req.Timeout = 3600000;
+				using (var response = req.GetResponse())
+				{
+					using (var responseStream = response.GetResponseStream())
+					{
+						var buffer = new byte[BUFFER_SIZE];
+						int bytesRead;
+						do
+						{
+							bytesRead = responseStream.Read(buffer, 0, BUFFER_SIZE);
+							outputFileStream.Write(buffer, 0, bytesRead);
+						} while (bytesRead > 0);
+					}
+				}
+			}
 		}
 	}
 }
